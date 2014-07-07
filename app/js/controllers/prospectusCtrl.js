588bcc060249300a46ac5ac54f61fcf4d5ed21c8
Kyle Olson <kolson@four51.com>
2014-07-07 09:12:43 -0500

    Email functionality

--- app/js/controllers/prospectusCtrl.js
+++ app/js/controllers/prospectusCtrl.js
@@ -24,7 +24,7 @@ function ($scope, $routeParams, $route, $location, $451, Product, ProductDisplay
         };
 
         Variant.save(variant, function(v) {
-            console.log(v);
+            sendEmail(v);
             $scope.step = 3;
         });
     }
@@ -34,6 +34,47 @@ function ($scope, $routeParams, $route, $location, $451, Product, ProductDisplay
             $scope.product.Specs.Mobile.Value = $scope.product.Specs.Mobile.Value.replace(/\D/g,'');
         }
     });
+
+    function sendEmail(variant) {
+        mandrill_client = new mandrill.Mandrill('wuy_yqU4xM44FjwrDenzGA');
+        var template_name = "bristol";
+        var firstName = variant.Specs['vFirstName'].Value;
+        //var toEmail = variant.Specs['Email'].Value;
+        var pdfURL = variant.ProductionURL;
+        var template_content = [
+            {
+                "name": 'header',
+                "content": 'Hello ' + firstName
+            },
+            {
+                "name": 'body',
+                "content": '<a href="' + pdfURL + '" target="_blank">Download Now!</a>'
+            }
+        ];
+        var message = {
+            'subject': 'Test email subject',
+            'from_email': 'testmail@four51.com',
+            'from_name': 'Test User',
+            'to': [{
+                /*'email': toEmail,*/
+                'email': 'kolson@four51.com',
+                'name': 'Kyle Olson',
+                'type': 'to'
+            }],
+            'headers': {
+                'Reply-To': 'demomail@four51.com'
+            },
+            'important': false
+        };
+        var async = false;
+        var ip_pool = "Main Pool";
+
+        mandrill_client.messages.sendTemplate({"template_name": template_name, "template_content": template_content, "message": message, "async": async, "ip_pool": ip_pool}, function(result) {
+            console.log(result);
+        }, function(e) {
+            console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+        });
+    }
 
 }]);
 
