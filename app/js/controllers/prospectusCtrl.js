c7bfb56f2592ffc9272d0b943dddf80388794153
Kyle Olson <kolson@four51.com>
2014-09-17 15:53:16 -0500

    Moved 'send' to service

--- app/js/controllers/prospectusCtrl.js
+++ app/js/controllers/prospectusCtrl.js
@@ -1,5 +1,5 @@
-four51.app.controller('ProspectusCtrl', ['$scope', '$routeParams', '$route', '$location', '$451', 'Product', 'ProductDisplayService', 'Order', 'Variant', 'User',
-function ($scope, $routeParams, $route, $location, $451, Product, ProductDisplayService, Order, Variant, User) {
+four51.app.controller('ProspectusCtrl', ['$scope', '$routeParams', '$route', '$location', '$451', 'Product', 'ProductDisplayService', 'Order', 'Variant', 'Prospectus',
+function ($scope, $routeParams, $route, $location, $451, Product, ProductDisplayService, Order, Variant, Prospectus) {
 
     $scope.step = 1;
 
@@ -26,55 +26,7 @@ function ($scope, $routeParams, $route, $location, $451, Product, ProductDisplay
     };
 
     function sendEmail(variant) {
-        //mandrill_client = new mandrill.Mandrill('wuy_yqU4xM44FjwrDenzGA'); //Four51 Test Key
-        //mandrill_client = new mandrill.Mandrill('SzIKUx5tFAs7Xse7UzvBiQ'); //Four51 Live Key
-
-        //mandrill_client = new mandrill.Mandrill('Ke6eCmBhFJDpxR65jD4fcw'); //Accent Test Key
-        mandrill_client = new mandrill.Mandrill('3oXmWhr-hEQqwzqawyH_dQ'); //Accent Live Key
-
-        var template_name = "bristol";
-        var firstName = variant.Specs['vFirstName'].Value;
-        var lastName = variant.Specs['vLastName'].Value;
-        var fullName = firstName + " " + lastName;
-        var toEmail = variant.Specs['Email'].Value;
-        var pdfURL = variant.ProductionURL.replace('web.four51.com','.four51.com');
-        var logo = $scope.user.Company.LogoUrl.replace('qa.four51','www.four51');
-        var template_content = [
-            {
-                "name": 'header',
-                "content": 'Hello ' + firstName
-            },
-            {
-                "name": 'body',
-                "content": '<a style="display: block; text-decoration: none; color: #FFF; background-color: #38c0ff; text-align: center; border-radius: 8px; padding: 5px;" href="' + pdfURL + '" target="_blank">Download my prospectus</a>'
-            },
-            {
-                "name": 'logo',
-                "content": '<img src="' + logo + '" style="max-width:100%; padding-bottom:15px;"/>'
-            }
-        ];
-        var message = {
-            'subject': 'Your personalised prospectus from City of Bristol College',
-            'from_email': 'test@accent.com',
-            'from_name': 'City of Bristol College',
-            'to': [{
-                'email': toEmail,
-                'name': fullName,
-                'type': 'to'
-            }],
-            'headers': {
-                'Reply-To': 'test@accent.com'
-            },
-            'important': false
-        };
-        var async = false;
-        var ip_pool = "Main Pool";
-
-        mandrill_client.messages.sendTemplate({"template_name": template_name, "template_content": template_content, "message": message, "async": async, "ip_pool": ip_pool}, function(result) {
-            console.log(result);
-        }, function(e) {
-            console.log('A Mandrill error occurred: ' + e.name + ' - ' + e.message);
-        });
+        Prospectus.send(variant, $scope.user);
     }
 
 }]);
--- app/js/services/prospectusService.js
+++ app/js/services/prospectusService.js
@@ -1,29 +1,62 @@
 four51.app.factory('Prospectus', ['$resource', '$451', function($resource, $451) {
-    function _then(fn, data) {
+    /*function _then(fn, data) {
         if (angular.isFunction(fn))
             fn(data);
-    }
+    }*/
 
-    function buildCacheID(order) {
-        var cacheID = "451Cache.Shippers." + order.ID;
-        angular.forEach(order.LineItems, function(item) {
-            cacheID += item.Quantity + item.Product.InteropID + item.ShipAddressID;
-        });
-        return cacheID;
-    }
+    var _send = function(variant, user) {
+        //mandrill_client = new mandrill.Mandrill('wuy_yqU4xM44FjwrDenzGA'); //Four51 Test Key
+        //mandrill_client = new mandrill.Mandrill('SzIKUx5tFAs7Xse7UzvBiQ'); //Four51 Live Key
 
-    var _query = function(order, success) {
-        if (!order) return null;
-        //var id = buildCacheID(order),
-        //    shippers = store.get(id);
-        //shippers ? _then(success, shippers) :
-        $resource($451.api('shipper')).query().$promise.then(function(list) {
-            //        store.set(id, list);
-            _then(success, list);
+        //mandrill_client = new mandrill.Mandrill('Ke6eCmBhFJDpxR65jD4fcw'); //Accent Test Key
+        mandrill_client = new mandrill.Mandrill('3oXmWhr-hEQqwzqawyH_dQ'); //Accent Live Key
+
+        var template_name = "bristol";
+        var firstName = variant.Specs['vFirstName'].Value;
+        var lastName = variant.Specs['vLastName'].Value;
+        var fullName = firstName + " " + lastName;
+        var toEmail = variant.Specs['Email'].Value;
+        var pdfURL = variant.ProductionURL.replace('web.four51.com','.four51.com');
+        var logo = user.Company.LogoUrl.replace('qa.four51','www.four51');
+        var template_content = [
+            {
+                "name": 'header',
+                "content": 'Hello ' + firstName
+            },
+            {
+                "name": 'body',
+                "content": '<a style="display: block; text-decoration: none; color: #FFF; background-color: #38c0ff; text-align: center; border-radius: 8px; padding: 5px;" href="' + pdfURL + '" target="_blank">Download my prospectus</a>'
+            },
+            {
+                "name": 'logo',
+                "content": '<img src="' + logo + '" style="max-width:100%; padding-bottom:15px;"/>'
+            }
+        ];
+        var message = {
+            'subject': 'Your personalised prospectus from City of Bristol College',
+            'from_email': 'test@accent.com',
+            'from_name': 'City of Bristol College',
+            'to': [{
+                'email': toEmail,
+                'name': fullName,
+                'type': 'to'
+            }],
+            'headers': {
+                'Reply-To': 'test@accent.com'
+            },
+            'important': false
+        };
+        var async = false;
+        var ip_pool = "Main Pool";
+
+        mandrill_client.messages.sendTemplate({"template_name": template_name, "template_content": template_content, "message": message, "async": async, "ip_pool": ip_pool}, function(result) {
+            console.log(result);
+        }, function(e) {
+            console.log('A Mandrill error occurred: ' + e.name + ' - ' + e.message);
         });
-    }
+    };
 
     return {
-        query: _query
+        send: _send
     }
 }]);
